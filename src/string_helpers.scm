1f29181a34d397bb0f944b31f1f24018a970ce57
Joshua Barrett <joshua@barrettoc.com>
2015-12-15 12:09:39 -0500

    cleaned and optimized string-helpers

--- src/string_helpers.scm
+++ src/string_helpers.scm
@@ -1,13 +1,13 @@
 ; Just some helpers for playing with strings
-;
 (declare (unit string-helpers))
-(require-extension srfi-13) ; string goodies
+(use srfi-13) ; string goodies
 
 (define (string-tail str)
-  (let ((chars (string->list str)))
-    (cond ((null? chars) "")
-          (else (list->string (cdr chars))))))
+  (if (equal? str "")
+      ""
+      (string-drop str 1)))
 
 (define (string-head str)
-  (cond ((equal? str "") '())
-        (else (make-string 1 (car (string->list str))))))
+  (if (equal? str "")
+      '()
+      (string-take str 1)))
